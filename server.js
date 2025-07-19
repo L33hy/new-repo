3176c04d22910b77cc53584675c828de809f76fb
L33hy <Lehidaniel99@gmail.com>
2025-07-19 14:50:31 +0100

    Created the demo and development files for the cse340 course

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,18 +14,30 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.set("views", "./views")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+/* ***********************
+ * Routes
+ *************************/
+// Index route
 app.use(static)
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || 'localhost'
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+  console.log('Press Ctrl+C to quit.')
 })
