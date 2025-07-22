9c83579857baf6b679403ca667adfd7da69dc739
L33hy <Lehidaniel99@gmail.com>
2025-07-22 12:39:55 +0100

    Created the file and folders for the web page.

--- routes/static.js
+++ routes/static.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static(path.join(__dirname, '../public')));
+router.use(express.static(path.join(__dirname, "../public")));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
